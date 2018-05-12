9a81a3e7979a8fffdac92fd5040f2972b56e2e09
ejnarvala <ejnar@Ejnar-Laptop.localdomain>
2018-05-11 22:36:44 -0400

    modified extractor to deal with more internal error handling

--- extractor.py
+++ extractor.py
@@ -3,7 +3,11 @@ import re
 
 
 def extract_leagueId(url):
-	parsed = urlparse.urlparse(url)
-	leagueId_str = (urlparse.parse_qs(parsed.query)['leagueId'])[0] #throws KeyError
-	leagueId = int(re.sub("[^0-9]", "", leagueId_str)); #throws ValueError
-	return leagueId
+	try:
+		parsed = urlparse.urlparse(url)
+		leagueId_str = (urlparse.parse_qs(parsed.query)['leagueId'])[0] #throws KeyError
+		leagueId = int(re.sub("[^0-9]", "", leagueId_str)); #throws ValueError
+	except (ValueError, KeyError) as e:
+		error = 'Invalid URL'
+		return (False, error)
+	return (True, leagueId)
