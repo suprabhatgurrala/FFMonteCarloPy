b95d09b2c639453dab2360d87ef68cda7ff2ada2
Ejnar Arechavala <ejnar123@gmail.com>
2018-10-02 20:04:45 -0400

    tabsss

--- exceptions.py
+++ exceptions.py
@@ -1,19 +1,19 @@
 class URLError(Exception):
-    def __init__(self,*args,**kwargs):
-        Exception.__init__(self,*args,**kwargs)
-    def __str__(self):
-    	return 'URL Error: ' + super().__str__()
+	def __init__(self,*args,**kwargs):
+		Exception.__init__(self,*args,**kwargs)
+	def __str__(self):
+		return 'URL Error: ' + super().__str__()
 
 class IDError(Exception):
-    def __init__(self,*args,**kwargs):
-        Exception.__init__(self,*args,**kwargs)
-    def __str__(self):
-    	return 'ID Error: ' + super().__str__()
+	def __init__(self,*args,**kwargs):
+		Exception.__init__(self,*args,**kwargs)
+	def __str__(self):
+		return 'ID Error: ' + super().__str__()
 
 
 
 class SimulatorError(Exception):
-    def __init__(self,*args,**kwargs):
-        Exception.__init__(self,*args,**kwargs)
-    def __str__(self):
-    	return 'Simulator Error: ' + super().__str__()
+	def __init__(self,*args,**kwargs):
+		Exception.__init__(self,*args,**kwargs)
+	def __str__(self):
+		return 'Simulator Error: ' + super().__str__()
